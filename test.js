0ab6b1825c8f873211843b558a52492890e4323d
oscarjoker10 <sibayoscar@hotmail.com>
2017-04-25 13:51:04 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,52 +1,97 @@
-var Height = window.innerHeight*.97
-var Width = window.innerWidth*.99
+var Height =680 //window.innerHeight*.97
+var Width =1080// window.innerWidth*.99
 var hud
 var blocks=[]
 var blockH=40
 var blockW=70.3
 var pad 
+var right,left
+var size
+var speedx,speedy
 function preload()
 {
-for (var x =0; x<9; x++)
+for (var x =0; x<8; x++)
 {
  blocks[x]=[]
  for(var y =0; y<Height; y++)
 {
- blocks[x][y]= new Block((x*(((Width*.3)+(Width*.7))/2)/9)+Width*.3,y*blockH,y,(((Width*.3)+(Width*.7))/2)/9)
+
+ blocks[x][y]= new Block((x*(((Width*.3)+(Width*.7))/2)/10)+Width*.3,y*((Width*.3)+(Width*.7)/2)/25,y,(((Width*.3)+(Width*.7))/2)/10)
 }
 
 }
-noLoop();
 }
 function setup()
 {
+speedx=speedy=6
  createCanvas(Width,Height)
+frameRate(60);
+smooth();
  hud= new Hud();
  pad = new Paddle((((Width*.3)+(Width*.7))/2),Height*.95);
 }
 function draw()
 {
+
 background(225)
 hud.draw()
-keyDown()
-pad.draw()
 
-}
-function keyDown()
+for (var y=0;y<9;y++)
 {
-if (keyIsDown(65))
-pad.x-=1
+for(var x=0; x<9; x++)
+	{
+	// blocks[x][y].draw()
+
+	}
 }
+pad.draw()
+ball();
+}
+function ball()
+{
+this.x=Width/2+speedx
+this.y=Height/2+speedy
+fill(0)
+ ellipse(this.x,this.y,20,20)
+if(this.y<0)
+speedy+=8
+if(this.y>Height)
+speedy-=8
+}
+
+
+
+
+function keyPressed()
+{
+ if(key=='a'||key=='A')
+ {
+ left= true
+ }
+  if(key=='d'||key=='D')
+ {
+ right = true
+ }
+}
+function keyReleased()
+{
+ if(key=='a'||key=='A')
+ {
+ left= false
+ }
+  if(key=='d'||key=='D')
+ {
+ right = false
+ }
+}
+
 function Hud()
 {
  this.draw = function()
 	{
 	line((Width*.3),Height,(Width*.3),0)
 	line((Width*.7),Height,(Width*.7),0)
-	for(var x=0; x<9; x++)
-	{
-	 blocks[x][1].draw()
-	}
+	
 	}
 }
 function Block(x,y,l,s)
@@ -55,13 +100,14 @@ this.x=x
 this.y=y
 this.num=l+1
 this.size=s
+size=this.s
 this.draw = function()
 {
 fill(255,0,0)
-rect(this.x,this.y+30,(((Width*.3)+(Width*.7))/2)/9,this.s)
+rect(this.x,this.y,this.size,this.size/2)
 fill(0)
-textSize(15)
-text(this.size,this.x,this.y+25)
+//textSize(15)
+//text(this.size,this.x,this.y)
 }
 this.update = function()
 {
@@ -72,10 +118,16 @@ function Paddle(x,y)
 {
 this.x=x
 this.y=y
+
+this.size=blocks[0][0].size*2
 this.draw = function()
 {
+if(left==true&&this.x>Width*.3)
+this.x-=7
+if(right==true&&this.x<(Width*.7)-111)
+this.x+=7
 fill(0)
-rect(this.x,this.y,100,30)
+rect(this.x,this.y,this.size,this.size/4)
 }
 this.update = function()
 {
